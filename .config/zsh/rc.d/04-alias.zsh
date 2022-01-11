bee0bd08729fa6064d1fb2fa6b4dbbfa4db1bf06
Waldemar Schroeer <waldemar.schroeer@de.ttiinc.com>
2022-01-11 11:30:44 +0100

    Don't use icons on 'ls'

--- .config/zsh/rc.d/04-alias.zsh
+++ .config/zsh/rc.d/04-alias.zsh
@@ -9,8 +9,8 @@
 # +-------------------------------------------------------------------------+
 
 if ! type "exa" &> /dev/null; then
-    alias l='exa -lghm --icons --group-directories-first --git'
-    alias ll='exa -laghm --icons --group-directories-first --git'
+    alias l='exa -lghm --group-directories-first --git'
+    alias ll='exa -laghm --group-directories-first --git'
 else
     alias l='LC_ALL=C ls --color=auto --group-directories-first -lh'
     alias ll='LC_ALL=C ls --color=auto --group-directories-first -lah'
